5060378d399e1d15382181ef5e0937e565f0c85d
nick-echevarria <nick.echev@gmail.com>
2020-05-16 14:45:20 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+    state = { 
+        timesClicked: 0
+    }
+
+    updateClickCount = () => { 
+        this.setState(previousState => { 
+            return { 
+                timesClicked: previousState.timesClicked + 1
+            }
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <button onClick={this.updateClickCount}>{this.state.timesClicked}</button>
+            </div>
+        );
+    }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,45 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+class YouTubeDebugger extends Component {
+    state = { 
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8,
+            video: {
+                resolution: '1080p'
+            }
+        }
+    }
+
+    changeBitrate = () => { 
+        this.setState({
+            settings: Object.assign({},
+                this.state.settings, {
+                bitrate: 12
+            })
+        })
+    }
+
+    changeResolution = () => {
+        this.setState({
+            settings: Object.assign({}, this.state.settings, {
+              video: Object.assign({}, this.state.settings.video, {
+                resolution: '720p'
+              })
+            })
+        }) // to use an Object.assign to change a deeply nested object, 
+        //you'll need to call it on each level of the object
+    }
+    
+    render() {
+        return (
+            <div>
+                <button className="bitrate" onClick={this.changeBitrate}>Change Bitrate</button>
+                <button className="resolution" onClick={this.changeResolution}>Change Resolution</button>
+            </div>
+        );
+    }
+}
+
+export default YouTubeDebugger;
